15854da7e3e4c74994fc781344d0f073cd3988d3
samuelxd14 <samuel.ruiz1607@gmail.com>
2026-05-12 19:39:07 -0300

    elimino archivo duplicado

--- pv_tp3_grupo21/src/services/proyectoService.js
+++ pv_tp3_grupo21/src/services/proyectoService.js
@@ -1,19 +1,27 @@
-const proyectoService = (() => {
+const proyectoService = ( () => {
     let proyectos = [
-    {id: 1, titulo: "Turnos online", categoria: "Web", estado: "En curso"},
-    {id: 2, titulo: "App control de asistencia", categoria: "Mobile", estado: "En curso"},
-    {id: 3, titulo: "Gestion de biblioteca", categoria: "Escritorio", estado: "Finalizado"},
-    {id: 4, titulo: "Sistema de cuentas", categoria: "Web", estado: "En curso"},
-    {id: 5, titulo: "App de fotoshop", categoria: "Mobile", estado: "Finalizado"},
-    {id: 6, titulo: "Sistema de ventas", categoria: "Escritorio", estado: "En curso"},
+        {id: 1, titulo: "Turnos online", categoria: "Web", estado: "En curso"},
+        {id: 2, titulo: "App Control de asistencia", categoria: "Mobile", estado: "En curso"},
+        {id: 3, titulo: "Gestion de biblioteca", categoria: "Escritorio", estado: "Finalizado"},
+        {id: 4, titulo: "API Gestion de alumnos", categoria: "Backend", estado: "En curso"},
+        {id: 5, titulo: "", categoria: "", estado: "Finalizado"}
     ];
+
     const obtenerProyectos = () => [...proyectos];
+
+    const agregarProyecto = (p) => {
+        proyectos.push(p);
+    };
 
     const eliminarProyecto = (id) => {
         proyectos = proyectos.filter(p => p.id !== id);
-    }
-    return {obtenerProyectos, eliminarProyecto};
+    };
 
-    })()
+    const buscarProyecto = (texto) => {
+        proyectos.filter(p => p.titulo.toLowerCase().includes(texto.toLowerCase()));
+    };
+
+    return {obtenerProyectos, agregarProyecto, eliminarProyecto, buscarProyecto};
+})();
 
 export default proyectoService;
